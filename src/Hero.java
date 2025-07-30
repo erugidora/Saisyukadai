34777c79177914ab2e94bdcccbece22d43eeea47
erugidora <241009@std.hi-joho.ac.jp>
2025-07-30 10:26:12 +0900

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,17 +1,17 @@
 public class Hero extends Character {
-    private String getweapon;
+    private String getWeapon;
 
     public Hero(String name, int hp, String weapon) {
         super(name, hp);
-        this.getweapon = weapon;
+        this.getWeapon = weapon;
     }
 
     public void attack(Creature target) {
-        System.out.println(this.getName() + "は" + getweapon + "で攻撃！" + target.getName() + "に10のダメージを与えた！");
+        System.out.println(this.getName() + "は" + getWeapon + "で攻撃！" + target.getName() + "に10のダメージを与えた！");
         target.setHp(target.getHp() - 10);
     }
 
     public String getWeapon() {
-        return getweapon;
+        return getWeapon;
     }
 }
